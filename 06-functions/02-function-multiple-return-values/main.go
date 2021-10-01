92be518b71be6855d8a1f6c43bfed31684717668
MacTech9860 <39257815+shaukatmakandar786@users.noreply.github.com>
2021-10-01 18:08:21 +0530

    Create main.go

---
+++ 06-functions/02-function-multiple-return-values/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func arithmeticOperations(x,y int)(int,int){
+
+	add:=x+y
+	mul:=x*y
+	
+	return add,mul
+}
+func main() {
+	x := 10
+	y := 5
+
+	add,mul := arithmeticOperations(x, y)
+
+	fmt.Printf("Addition if %d and %d is %d\n", x, y, add)
+	fmt.Printf("Multiplicatio if %d and %d is %d\n", x, y, mul)
+
+}
+/*
+
+Addition if 10 and 5 is 15
+Multiplicatio if 10 and 5 is 50
+
+*/
