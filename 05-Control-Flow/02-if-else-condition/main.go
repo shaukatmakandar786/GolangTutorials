acd6f1ab1bdb56c72574a38006032111cafbb9c7
MacTech9860 <39257815+shaukatmakandar786@users.noreply.github.com>
2021-10-01 17:23:47 +0530

    Create main.go

---
+++ 05-Control-Flow/02-if-else-condition/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if age := 19; age >= 18 {
+
+		fmt.Println("You're eligible to vote")
+	} else {
+		fmt.Println("You're not eligible to vote")
+	}
+  
+}
+
+/*
+
+You're eligible to vote
+
+
+*/
