22ef6af537e2a4f79fe9cd2da3e21019d1cb234a
MacTech9860 <39257815+shaukatmakandar786@users.noreply.github.com>
2021-10-01 20:55:00 +0530

    Create app.go

---
+++ 07-packages/myapp/app.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+          "fmt"
+          
+)
+
+func main(){
+ 
+  if number.IsPrime(19){
+    
+    fmt.Println("Given number is prime")
+  }else{
+   
+    fmt.Println("give number is not a prime")
+  }
+  
+  fmt.Println(greeting.WelcomeText)
+  
+  fmt.Println(strings.Reverse("ShaukatMakandar"))
+  
+ 	fmt.Println(str.Count("Go is Awesome. I love Go", "Go"))
+
+
+  
+}
