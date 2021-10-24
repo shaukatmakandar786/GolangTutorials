75906fd03419a379678c73c64238fd101f9fca7a
shaukatmakandar786 <shaukatmakandar786@gmail.com>
2021-10-24 12:23:49 +0530

    take input from the user

---
+++ 08-take-input-from-the-user/01-scanf/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string
+	var a int
+	var b int
+	fmt.Print("Enter your name: ")
+	fmt.Scanf("%s", &name)
+	fmt.Println("Hello", name)
+
+	fmt.Println("enter value of a and b\n")
+	fmt.Scanf("%d%d",&a,&b)
+	fmt.Println("Addition of two number is",(a+b))
+}
+
+
---
+++ 08-take-input-from-the-user/02-NewReader/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter your name: ")
+
+	name, _ := reader.ReadString('\n')
+	fmt.Printf("Hello %s\n", name)
+}
